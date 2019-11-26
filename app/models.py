d9ff426c3807da61bbde92defbb8bbc0b5746f09
Victorvick399 <victorvick399@gmail.com>
2019-11-26 08:20:58 +0300

    remove a column in Pitch table

--- app/models.py
+++ app/models.py
@@ -59,7 +59,6 @@ class Pitch(db.Model):
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     description = db.Column(db.String(), index=True)
     title = db.Column(db.String())
     date_posted = db.Column(db.DateTime , nullable = False , default = datetime.utcnow)
