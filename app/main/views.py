4c006814f4077f1a911d2e356f3ae6294e6dee96
Victorvick399 <victorvick399@gmail.com>
2019-11-26 09:25:36 +0300

    change the name of template to be rendered

--- app/main/views.py
+++ app/main/views.py
@@ -97,7 +97,7 @@ def account():
         form.email.data = current_user.email
     image_file = url_for(
         'static', filename='profile_pics/' + current_user.image_file)
-    return render_template('profile.html', title='Account',
+    return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
 
 
