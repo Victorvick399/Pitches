33748f8e29c3c213f9f2ec3d31d242844b4c6a64
Victorvick399 <victorvick399@gmail.com>
2019-11-27 18:05:20 +0300

    add test runner

--- manage.py
+++ manage.py
@@ -11,10 +11,16 @@ app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
 
-manager.add_command('server',Server)
-
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @manager.shell
 def make_shell_context():
